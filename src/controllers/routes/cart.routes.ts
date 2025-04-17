cf9ad2ca3db6855e57550437a3321b2aef87ab5e
SantiagoAlvPer <santiago.alvarezpereira@unicolombo.edu.co>
2025-04-17 18:56:34 -0500

    route update

--- src/controllers/routes/cart.routes.ts
+++ src/controllers/routes/cart.routes.ts
@@ -2,6 +2,8 @@ import { Producer } from "../../shared/events/producers/cart.producer";
 import { AddTocartInput } from "../../shared/interfaces/addtocart";
 import { CartService } from "../../services/cart-service/cartSevice";
 import { Router, Request, Response } from "express";
+import { EventModel } from "../../models/cartEvent.model";
+import { CartItemModel } from "../../models/cartItem.model";
 
 
 const router = Router();
@@ -38,6 +40,21 @@ router.delete("/api/cart/items/:id", async (req: Request, res: Response): Promis
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Error al eliminar producto del carrito" });
+  }
+});
+
+router.get('/events', async (req, res) => {
+  const events = await EventModel.find().limit(10);
+  res.json(events);
+});
+
+router.get("/cart-items", async (req, res) => {
+  try {
+    const cartItems = await CartItemModel.find();
+    res.status(200).json(cartItems);
+  } catch (error) {
+    console.error("❌ Error al obtener los carritos:", error);
+    res.status(500).json({ message: "Error al obtener los carritos" });
   }
 });
 
